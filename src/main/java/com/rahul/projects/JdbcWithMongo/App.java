7050aacb0c549f88c09adf406ec580edd6dc9705
Rahul Kumar <rk7870382701@gmail.com>
2024-03-30 03:14:35 +0530

    plugin added to use Java 9, trying with datetime, added update method

--- src/main/java/com/rahul/projects/JdbcWithMongo/App.java
+++ src/main/java/com/rahul/projects/JdbcWithMongo/App.java
@@ -1,45 +1,87 @@
 package com.rahul.projects.JdbcWithMongo;
 
-import com.mongodb.client.result.InsertOneResult;
-import com.mongodb.client.result.UpdateResult;
 import com.rahul.projects.JdbcWithMongo.services.CrudServices;
+import org.bson.Document;
 
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class App {
+
+    static final CrudServices crudServices = new CrudServices();
+
     public static void main(String[] args) {
+        // createRecord();
+        // updateGender();
+        // getStudentsInAgeRange();
+        getStudentsCreatedOnGivenDate();
+        //datetimeInGMT();
 
-          final CrudServices crudServices = new CrudServices();
-//          for(int i=1; i<=50; i++) {
-//             crudServices.saveStudent("Rahul"+i, 51+i,"Ranchi"+i,"Jharkhand"+i,"Mother"+i,"Father"+i);
-//          }
-         //System.out.println("Saved :: "+savedRecord);
+    }
+
+    public static void updateGender() {
+        crudServices.updatedGenderOfSpecificStudents();
+    }
+
+    public static void getStudentsInAgeRange() {
+        final List<Document> studentsWithinAgeRange = crudServices.getStudentsWithinAgeRange(25, 27);
+        for (Document document : studentsWithinAgeRange) {
+            System.out.println(document);
+        }
+    }
+
+    public static void getStudentsCreatedOnGivenDate() {
+
+        //2024-03-22T19:15:58.934+00:00
+        final Date date = new Date();
+        final List<Document> studentsWithinAgeRange = crudServices.getStudentsCreatedOnDate(String.valueOf(date));
+        for (Document document : studentsWithinAgeRange) {
+            System.out.println(document);
+        }
+    }
 
 
-       //final UpdateResult updateResult = crudServices.updateManyStudentsByName("Rahul", "SharmaJi");
-        //System.out.println("Number of records updated :: "+updateResult.getModifiedCount());
+    public static void createRecord() {
+        Map<String, Object> record = new HashMap<>();
+        record.put("firstname", "Kamini");
+        record.put("lastname", "Singh");
+        record.put("age", 20);
+        record.put("city", "Kondapur");
+        record.put("state", "UttarPradesh");
+        record.put("mother", "Jaya");
+        record.put("father", "Amitabh");
+
+        crudServices.saveStudent(record);
+
+    }
+
+    public static void datetimeInGMT(){
+        // Get the current date and time in UTC
+        Instant instant = Instant.now();
+
+        // Set the time zone to GMT
+        ZoneId zone = ZoneId.of("GMT");
+
+        // Format the date and time as a string
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        String dateTime = instant.atZone(zone).format(formatter);
+
+        System.out.println("Current date and time (GMT): " + dateTime); //2024-03-29T21:18:11.031Z
 
 
-        //crudServices.findStudentsByName("Rahul");
-        //2024-02-06T19:12:59.997+00:00
+        //SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+        //format.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-        Date date = new Date("2024-02-06T19:12:59.997+00:00");
+    }
 
-        // getting the timestamp object
-        Timestamp ts = new Timestamp(date.getTime());
-
-        // using SimpleDateFormat class,we can format the
-        // time-stamp according to ourselves
-        // getting the timestamp upto sec
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-        sdf.format(ts);
-        crudServices.findStudentCreatedOnGivenDates(ts);
-
+    public void test2(){
+        Calendar currentDateTime = Calendar.getInstance();
+        currentDateTime.add(Calendar.MILLISECOND, -currentDateTime.getTimeZone().getOffset(currentDateTime.getTimeInMillis()));
+        Date date2 = currentDateTime.getTime();
+        System.out.println("Current GMT time in India :: "+date2); //Fri Mar 29 20:57:30 IST 2024 (GMT time hain ye)
 
     }
 }
--- src/main/java/com/rahul/projects/JdbcWithMongo/services/CrudServices.java
+++ src/main/java/com/rahul/projects/JdbcWithMongo/services/CrudServices.java
@@ -1,12 +1,20 @@
 package com.rahul.projects.JdbcWithMongo.services;
 
-import com.mongodb.client.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.client.result.UpdateResult;
 import com.rahul.projects.JdbcWithMongo.utils.MongoDBConnection;
 import org.bson.Document;
 
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CrudServices {
 
@@ -16,28 +24,102 @@ public class CrudServices {
         database = MongoDBConnection.getDBConnection();
     }
 
-    public InsertOneResult saveStudent(String name, Integer roll, String city, String state, String mother, String father) {
+    public void saveStudent(Map<String, Object> record) {
 
         MongoCollection<Document> collection = database.getCollection("students");
         Document document = new Document();
-        document.put("student_name", name);
-        document.put("student_roll", roll);
+        document.put("firstname", record.get("firstname"));
+        document.put("lastname", record.get("lastname"));
+        document.put("age", record.get("age"));
 
         //Address
         final Document addressDetails = new Document();
-        addressDetails.put("city", city);
-        addressDetails.put("state", state);
-        document.put("student_address", addressDetails);
+        addressDetails.put("city", record.get("city"));
+        addressDetails.put("state", record.get("state"));
+        document.put("address", addressDetails);
 
         //Parents
         final Document parentsDetails = new Document();
-        parentsDetails.put("mother", mother);
-        parentsDetails.put("father", father);
-        document.put("parents_details",parentsDetails);
+        parentsDetails.put("mother", record.get("mother"));
+        parentsDetails.put("father", record.get("father"));
+        document.put("parents", parentsDetails);
 
         document.put("creation_date", new Timestamp(System.currentTimeMillis()));
         final InsertOneResult insertOneResult = collection.insertOne(document);
-        return insertOneResult;
+        System.out.println("ID of record inserted :: " + insertOneResult.getInsertedId());
+
+    }
+
+    public void updatedGenderOfSpecificStudents() {
+        MongoCollection<Document> collection = database.getCollection("students");
+
+        //db.COLLECTION_NAME.update({SELECTION_CRITERIA}, {$set:{UPDATED_DATA}}
+
+        //db.contributor.find({name: {$in: ["Amit", "Suman"]}}).pretty()
+
+        Document selectionCriteria = new Document();
+        selectionCriteria.put("firstname", new Document("$in", List.of("Rahul", "Kundan", "Piyush", "Aman", "Mohit")));
+
+        Document updateQuery = new Document();
+        updateQuery.put("$set", new Document("gender", "Male"));
+
+        final UpdateResult updateResult = collection.updateMany(selectionCriteria, updateQuery);
+        System.out.println("Updated count :: " + updateResult.getModifiedCount());
+
+    }
+
+
+    //Fetching students in given age range
+    public List<Document> getStudentsWithinAgeRange(Integer lower, Integer higher) {
+        MongoCollection<Document> collection = database.getCollection("students");
+
+        List<Document> records = new ArrayList<>();
+
+        Document selectionCriteria = new Document();
+        selectionCriteria.put("age", new Document("$gte", lower).append("$lte", higher));
+
+        final FindIterable<Document> cursor = collection.find(selectionCriteria);
+
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            while (cursorIterator.hasNext()) {
+                final Document record = cursorIterator.next();
+                records.add(record);
+            }
+        }
+        System.out.println("Total records (documents) :: " + records.size());
+        return records;
+    }
+
+    //Fetching students in created on given date
+    public List<Document> getStudentsCreatedOnDate(String creationDate) {
+        String string = "2024-03-21T18:30:00.000+00:00";
+        final Instant instant = Instant.parse(string);
+        System.out.println("Parsed time :: "+instant);
+
+        final List<Document> allStudents = getAllStudents();
+        System.out.println("All :: "+allStudents.size());
+        System.out.println(allStudents.get(0).get("creation_date"));
+
+        final List<Document> response = allStudents.stream()
+                .filter(student ->
+                 student.get("creation_date").equals(instant))
+                .collect(Collectors.toList());
+
+        System.out.println("Filtered :: "+response.size());
+        return response;
+    }
+
+    public List<Document> getAllStudents() {
+
+        List<Document> records = new ArrayList<>();
+
+        MongoCollection<Document> collection = database.getCollection("students");
+        final MongoCursor<Document> cursorIterator = collection.find().cursor();
+        while (cursorIterator.hasNext()) {
+            final Document record = cursorIterator.next();
+            records.add(record);
+        }
+        return records;
     }
 
     public UpdateResult updateVeryFirstStudentByName(String oldName, String newName) {
@@ -75,7 +157,9 @@ public class CrudServices {
         MongoCollection<Document> collection = database.getCollection("students");
         Document searchQuery = new Document();
         searchQuery.put("student_name", studentName);
+
         FindIterable<Document> cursor = collection.find(searchQuery);
+        System.out.println("Records found :: " + cursor.showRecordId(true));
 
         try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
             while (cursorIterator.hasNext()) {
@@ -83,6 +167,24 @@ public class CrudServices {
             }
         }
     }
+
+    public void findStudentsRollGreaterThan(Integer roll) {
+        MongoCollection<Document> collection = database.getCollection("students");
+        Document searchField = new Document();
+        Document searchCondition = new Document();
+        searchCondition.put("$gte", roll);
+        searchField.put("student_roll", searchCondition);
+
+        FindIterable<Document> cursor = collection.find(searchField);
+
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            while (cursorIterator.hasNext()) {
+                System.out.println(cursorIterator.next());
+            }
+        }
+    }
+
+    //$gte
 
     public void deleteStudentsByName(String studentName) {
         MongoCollection<Document> collection = database.getCollection("students");
@@ -93,14 +195,13 @@ public class CrudServices {
     }
 
 
-    public void findStudentCreatedOnGivenDates(Timestamp timestamp){
+    public void findStudentCreatedOnGivenDates(Timestamp timestamp) {
         MongoCollection<Document> collection = database.getCollection("students");
         Document query = new Document();
-        query.put("creation_date",timestamp);
-
+        query.put("creation_date", timestamp);
 
         final FindIterable<Document> cursor = collection.find(query);
-        System.out.println("Records found :: "+cursor.showRecordId(true));
+        System.out.println("Records found :: " + cursor.showRecordId(true));
         try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
             while (cursorIterator.hasNext()) {
                 System.out.println(cursorIterator.next());
