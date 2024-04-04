f0e31ca554487ec7fa282eca920b99d126432295
Rahul Kumar <rk7870382701@gmail.com>
2024-04-04 16:50:52 +0530

    method added that uses multiple condition and & or operator to query db

--- src/main/java/com/rahul/projects/JdbcWithMongo/App.java
+++ src/main/java/com/rahul/projects/JdbcWithMongo/App.java
@@ -3,7 +3,7 @@ package com.rahul.projects.JdbcWithMongo;
 import com.rahul.projects.JdbcWithMongo.services.CrudServices;
 import org.bson.Document;
 
-import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -13,52 +13,66 @@ public class App {
 
     static final CrudServices crudServices = new CrudServices();
 
-    public static void main(String[] args) {
-        // createRecord();
-        // updateGender();
-        // getStudentsInAgeRange();
-        getStudentsCreatedOnGivenDate();
-        //datetimeInGMT();
+    public static void main(String[] args) throws ParseException {
+        // testCreateRecord();
+        // testUpdateGender();
+        // testGetStudentsInAgeRange();
+        // datetimeInGMT();
+        // testFindStudentCreatedOnBeforeGivenDates();
+        // testFindStudentLastUpdatedOnBeforeGivenDates();
+        testFindRecordsWithMultipleCondition();
 
     }
 
-    public static void updateGender() {
-        crudServices.updatedGenderOfSpecificStudents();
+    /*
+    Find records:
+    Gender must be Male
+    Age must be greater than 25
+    Last updated date or created date must be greater than "2024-03-04T05:45:01.758+00:00"
+     */
+    public static void testFindRecordsWithMultipleCondition() throws ParseException {
+        String expDate = "2024-02-04T05:44:31.182+00:00"; //Unparseable date: "2024-02-04T05:44:31.182+00:00"
+        expDate = "2024-02-25 09:28:04.401 UTC";
+
+        crudServices.findRecordsWithMultipleCondition(expDate);
     }
 
-    public static void getStudentsInAgeRange() {
+    public static void testCreateRecord() {
+        Map<String, Object> record = new HashMap<>();
+        record.put("firstname", "Sonali");
+        record.put("lastname", "Singh");
+        record.put("age", 25);
+        record.put("gender", "Female");
+        record.put("city", "Kolkata");
+        record.put("state", "WestBengal");
+        record.put("mother", "Ram");
+        record.put("father", "Sita");
+        crudServices.saveStudent(record);
+    }
+
+    public static void testFindStudentCreatedOnBeforeGivenDates() throws ParseException {
+        crudServices.findStudentCreatedOnBeforeGivenDates(null);
+
+    }
+
+    public static void testFindStudentLastUpdatedOnBeforeGivenDates() throws ParseException {
+        crudServices.findStudentLastUpdatedOnBeforeGivenDates(null);
+
+    }
+
+    public static void testGetStudentsInAgeRange() {
         final List<Document> studentsWithinAgeRange = crudServices.getStudentsWithinAgeRange(25, 27);
         for (Document document : studentsWithinAgeRange) {
             System.out.println(document);
         }
     }
 
-    public static void getStudentsCreatedOnGivenDate() {
-
-        //2024-03-22T19:15:58.934+00:00
-        final Date date = new Date();
-        final List<Document> studentsWithinAgeRange = crudServices.getStudentsCreatedOnDate(String.valueOf(date));
-        for (Document document : studentsWithinAgeRange) {
-            System.out.println(document);
-        }
+    public static void testUpdateGender() {
+        crudServices.updatedGenderOfSpecificStudents();
     }
 
 
-    public static void createRecord() {
-        Map<String, Object> record = new HashMap<>();
-        record.put("firstname", "Kamini");
-        record.put("lastname", "Singh");
-        record.put("age", 20);
-        record.put("city", "Kondapur");
-        record.put("state", "UttarPradesh");
-        record.put("mother", "Jaya");
-        record.put("father", "Amitabh");
-
-        crudServices.saveStudent(record);
-
-    }
-
-    public static void datetimeInGMT(){
+    public static void datetimeInGMT() {
         // Get the current date and time in UTC
         Instant instant = Instant.now();
 
@@ -77,11 +91,11 @@ public class App {
 
     }
 
-    public void test2(){
+    public void test2() {
         Calendar currentDateTime = Calendar.getInstance();
         currentDateTime.add(Calendar.MILLISECOND, -currentDateTime.getTimeZone().getOffset(currentDateTime.getTimeInMillis()));
         Date date2 = currentDateTime.getTime();
-        System.out.println("Current GMT time in India :: "+date2); //Fri Mar 29 20:57:30 IST 2024 (GMT time hain ye)
+        System.out.println("Current GMT time in India :: " + date2); //Fri Mar 29 20:57:30 IST 2024 (GMT time hain ye)
 
     }
 }
--- src/main/java/com/rahul/projects/JdbcWithMongo/services/CrudServices.java
+++ src/main/java/com/rahul/projects/JdbcWithMongo/services/CrudServices.java
@@ -10,11 +10,12 @@ import com.rahul.projects.JdbcWithMongo.utils.MongoDBConnection;
 import org.bson.Document;
 
 import java.sql.Timestamp;
-import java.time.Instant;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CrudServices {
 
@@ -24,6 +25,82 @@ public class CrudServices {
         database = MongoDBConnection.getDBConnection();
     }
 
+
+    public void findRecordsWithMultipleCondition(String dateStr) throws ParseException {
+        MongoCollection<Document> collection = database.getCollection("students");
+        List<Document> criteriaList = new ArrayList<>();  //Updated :: Command failed with error 2 (BadValue): '$and must be an array'
+        criteriaList.add(new Document("gender", new Document("$eq","Male")));
+        criteriaList.add(new Document("age", new Document("$gt", 25)));
+
+        Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS ZZZ").parse(dateStr);
+        Document query1 = new Document("created_on",new Document("$lt", date));
+        Document query2 = new Document("last_updated_on",new Document("$lt", date));
+        List<Document> dateCriteriaLists = new ArrayList<>();
+        dateCriteriaLists.add(query1);
+        dateCriteriaLists.add(query2);
+        criteriaList.add(new Document("$and", dateCriteriaLists));
+
+        Document finalQueryWithAND = new Document();
+        finalQueryWithAND.put("$and",criteriaList);
+
+        final FindIterable<Document> documents = collection.find(finalQueryWithAND);
+
+        System.out.println("== Records with age > 25 and gender 'Male' And CreationDate or UpdatedDate > 2024-02-25 09:28:04.401 UTC ==");
+        try (final MongoCursor<Document> cursorIterator = documents.cursor()) {
+            while (cursorIterator.hasNext()) {
+                System.out.println(cursorIterator.next());
+            }
+        }
+
+    }
+
+
+    //Older records created before the given dates
+    public void findStudentCreatedOnBeforeGivenDates(String dateStr) throws ParseException {
+        //creation_date : 2024-03-21T18:30:00.000+00:00
+        String expDate = "2024-02-25 09:28:04.401 UTC";
+        MongoCollection<Document> collection = database.getCollection("students");
+
+        Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS ZZZ").parse(expDate);
+        System.out.println("Date :: "+date);
+
+        Document query = new Document();
+        query.put("created_on", new Document("$lte", date));
+
+        final FindIterable<Document> cursor = collection.find(query);
+        System.out.println("===== Records created on before date :: "+date);
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            while (cursorIterator.hasNext()) {
+                System.out.println(cursorIterator.next());
+            }
+        }
+
+    }
+
+    //Older records last updated before the given dates
+    public void findStudentLastUpdatedOnBeforeGivenDates(String dateStr) throws ParseException {
+        //creation_date : 2024-03-21T18:30:00.000+00:00
+        String expDate = "2024-02-25 09:28:04.401 UTC";
+        MongoCollection<Document> collection = database.getCollection("students");
+
+        Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS ZZZ").parse(expDate);
+        System.out.println("Date :: "+date);
+
+        Document query = new Document();
+        query.put("last_updated_on", new Document("$lte", date));
+
+        final FindIterable<Document> cursor = collection.find(query);
+        System.out.println("====== Records last updated on before date :: "+expDate);
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            while (cursorIterator.hasNext()) {
+                System.out.println(cursorIterator.next());
+            }
+        }
+
+    }
+
+
+
     public void saveStudent(Map<String, Object> record) {
 
         MongoCollection<Document> collection = database.getCollection("students");
@@ -31,6 +108,7 @@ public class CrudServices {
         document.put("firstname", record.get("firstname"));
         document.put("lastname", record.get("lastname"));
         document.put("age", record.get("age"));
+        document.put("gender", record.get("gender"));
 
         //Address
         final Document addressDetails = new Document();
@@ -44,7 +122,8 @@ public class CrudServices {
         parentsDetails.put("father", record.get("father"));
         document.put("parents", parentsDetails);
 
-        document.put("creation_date", new Timestamp(System.currentTimeMillis()));
+        document.put("created_on", new Timestamp(System.currentTimeMillis()));
+        document.put("last_updated_on", new Timestamp(System.currentTimeMillis()));
         final InsertOneResult insertOneResult = collection.insertOne(document);
         System.out.println("ID of record inserted :: " + insertOneResult.getInsertedId());
 
@@ -58,10 +137,10 @@ public class CrudServices {
         //db.contributor.find({name: {$in: ["Amit", "Suman"]}}).pretty()
 
         Document selectionCriteria = new Document();
-        selectionCriteria.put("firstname", new Document("$in", List.of("Rahul", "Kundan", "Piyush", "Aman", "Mohit")));
+        selectionCriteria.put("firstname", new Document("$in", List.of("Kamini", "Sonali")));
 
         Document updateQuery = new Document();
-        updateQuery.put("$set", new Document("gender", "Male"));
+        updateQuery.put("$set", new Document("gender", "Female"));
 
         final UpdateResult updateResult = collection.updateMany(selectionCriteria, updateQuery);
         System.out.println("Updated count :: " + updateResult.getModifiedCount());
@@ -88,25 +167,6 @@ public class CrudServices {
         }
         System.out.println("Total records (documents) :: " + records.size());
         return records;
-    }
-
-    //Fetching students in created on given date
-    public List<Document> getStudentsCreatedOnDate(String creationDate) {
-        String string = "2024-03-21T18:30:00.000+00:00";
-        final Instant instant = Instant.parse(string);
-        System.out.println("Parsed time :: "+instant);
-
-        final List<Document> allStudents = getAllStudents();
-        System.out.println("All :: "+allStudents.size());
-        System.out.println(allStudents.get(0).get("creation_date"));
-
-        final List<Document> response = allStudents.stream()
-                .filter(student ->
-                 student.get("creation_date").equals(instant))
-                .collect(Collectors.toList());
-
-        System.out.println("Filtered :: "+response.size());
-        return response;
     }
 
     public List<Document> getAllStudents() {
@@ -178,8 +238,6 @@ public class CrudServices {
         }
     }
 
-    //$gte
-
     public void deleteStudentsByName(String studentName) {
         MongoCollection<Document> collection = database.getCollection("students");
         Document searchQuery = new Document();
@@ -188,19 +246,4 @@ public class CrudServices {
         collection.deleteOne(searchQuery);
     }
 
-
-    public void findStudentCreatedOnGivenDates(Timestamp timestamp) {
-        MongoCollection<Document> collection = database.getCollection("students");
-        Document query = new Document();
-        query.put("creation_date", timestamp);
-
-        final FindIterable<Document> cursor = collection.find(query);
-        System.out.println("Records found :: " + cursor.showRecordId(true));
-        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
-            while (cursorIterator.hasNext()) {
-                System.out.println(cursorIterator.next());
-            }
-        }
-
-    }
 }
